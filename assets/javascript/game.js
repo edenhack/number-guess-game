409d0c502ba8e9ad4de56f053ca6f6b3f30ae8d9
Kingsleigh Crown <20748544+edenhack@users.noreply.github.com>
2019-10-09 17:47:53 -0500

    If statements for guessing and losing added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,6 @@
 let randomQNum=0;
 let score=0;
+let chances = 3;
 
 //This generates the random number
 function randomNum(){
@@ -21,12 +22,29 @@ scoreBoard();
 //Pull user input key and run function
 document.onkeyup = function(event){
     const userInput = event.key();
+
+    if(chances = 0){
+        alert("You have run out of chances.");
+        alert("Your score was: " + score);
+        chances=3;
+        score=0;
+        randomNum();
+        scoreBoard();
+    }
+
     if (userInput === randomQNum){
         alert("Congrats that's the correct number");
         score++;
+        randomNum();
+        scoreBoard();
+        chances = 3;
     } else if (userInput < randomQNum) {
         alert("Your guess is less than the number.");
-    } else {
+        chances--;
+    } else if (userInput > randomQNum) {
         alert("Your guess is higher than the number.");
+        chances--;
+    } else {
+        alert("That is not a number, please try again.");
     }
 }
